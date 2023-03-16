44de480ba8d2470126cad585b297e21cfd944b23
Seth B Morgan <seth.b.morgan@gmail.com>
2023-03-16 15:49:23 -0400

    Update bf_data_2023_exploratory.R

--- analytics/BFRproj/bf_data_2023_exploratory.R
+++ analytics/BFRproj/bf_data_2023_exploratory.R
@@ -2,10 +2,8 @@
 library(readr)
 library(dplyr)
 library(ggplot2)
+library(stringr)
 
 #Import 2023 crop data
 bf_2023 <- read_csv("Brosey Farming - data_2023.csv")
 str(bf_2023)
-bf_2023
-
-#as.Date(bf_2023$sow_date)
