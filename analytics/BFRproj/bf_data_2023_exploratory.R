7c20bbe60ddd8b814e4e445e5fad7937d21e16dd
Seth B Morgan <seth.b.morgan@gmail.com>
2023-03-16 20:42:21 -0400

    Update to EA Rscript

--- analytics/BFRproj/bf_data_2023_exploratory.R
+++ analytics/BFRproj/bf_data_2023_exploratory.R
@@ -5,9 +5,17 @@ library(ggplot2)
 library(stringr)
 library(visdat)
 
-#import 2023 crop data
+#import 2023 bf crop data
 bf_2023 <- read_csv("Brosey Farming - data_2023.csv")
 str(bf_2023)
 
 #validate data
 vis_miss(bf_2023)
+
+#explore analysis
+bf_2023 %>% count(crop, sow_date)
+
+bf_2023 <- bf_2023 %>% mutate(sow_no_seed_tot= sow_no_cell* sow_no_seed_per)
+bf_2023 %>% count(crop, sow_date, wt= sow_no_seed_tot)
+
+ggplot(bf_2023, aes(x= crop, y= sow_no_seed_tot)) + geom_col()
